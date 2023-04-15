2f7b2f90afeaf1846a5226bf2cc183bc0f12d6e6
Администратор <@ALIM>
2023-04-15 16:25:09 +0600

    qqq

--- ProjectHack/Model/ProductSale.cs
+++ ProjectHack/Model/ProductSale.cs
@@ -3,9 +3,9 @@
     public class ProductSale
     {
         public int Id { get; set; }
-        public string Product { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public DateTime Time { get; set; }
+        public int BarCode { get; set; }
     }
 }
