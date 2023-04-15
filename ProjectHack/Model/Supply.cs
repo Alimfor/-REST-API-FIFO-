54dcde7829d11506f91e852db4c9adf3be9a58d2
Администратор <@ALIM>
2023-04-15 16:31:39 +0600

    qqq

--- ProjectHack/Model/Supply.cs
+++ ProjectHack/Model/Supply.cs
@@ -3,7 +3,7 @@
     public class Supply
     {
         public int Id { get; set; }
-        public string Product { get; set; }
+        public int BarCode { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public DateTime Time { get; set; }
